dd88ca1bda272f44b75939e05b9c3e0ddcb0083d
Gulara Gulmammadova <gularagulmammadova@gmail.com>
2025-04-12 18:26:26 +0400

    Create Email Screen

---
+++ lib/ui/screen/email/email_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class EmailScreen extends StatelessWidget {
+  const EmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Email Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email),
+              )
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+              },
+              child: Text('Login',
+              style: TextStyle(fontSize: 20),),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                backgroundColor: Colors.blue,
+                textStyle: TextStyle(fontSize: 20
+              ),
+            ),
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
